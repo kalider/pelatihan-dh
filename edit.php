96d1551cf8fea7759845a820a63e976b47d1d05a
Anang Permana <anang143@gmail.com>
2021-07-01 07:29:45 +0700

    tambah file edit

---
+++ edit.php
@@ -0,0 +1,71 @@
+<?php
+
+include 'koneksi.php';
+$id=$_GET['id'];
+$sql="SELECT * FROM santri WHERE id='$id'";
+$result = mysqli_query($connect, $sql);
+$santri = mysqli_fetch_array($result, MYSQLI_ASSOC);
+// var_dump($data_santri);die;
+mysqli_free_result($result);
+
+if(isset($_POST['edit'])){
+  $nama_depan = $_POST['nama_depan'];
+  $nama_belakang = $_POST['nama_belakang'];
+  $jenis_kelamin = $_POST['jenis_kelamin'];
+  $hp_orangtua = $_POST['hp_orangtua'];
+  $kelas = $_POST['kelas'];
+  $alamat = $_POST['alamat'];
+	$sql = "UPDATE santri SET nama_depan='$nama_depan', nama_belakang='$nama_belakang', jenis_kelamin='$jenis_kelamin', hp_orangtua='$hp_orangtua', kelas='$kelas', alamat='$alamat' WHERE id='$id'";
+
+	if (mysqli_query($connect, $sql)) {
+	  header("location:index.php");
+	} else {
+	  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+}
+mysqli_close($connect);
+ ?>
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Form Edit Santri</title>
+</head>
+<body>
+	<h3>Form Edit Data Santri</h3>
+	<p>Isi form di bawah ini</p>
+
+<form method="POST">
+  <label for="fname">Nama depan:</label><br>
+  <input type="text" id="fname" name="nama_depan" value="<?=$santri['nama_depan']?>"><br><br>
+
+  <label for="lname">Nama belakang:</label><br>
+  <input type="text" id="lname" name="nama_belakang" value="<?=$santri['nama_belakang']?>"><br><br>
+
+  <label>JK:</label><br><br>
+  <input type="radio" id="laki" name="jenis_kelamin" value="laki-laki" <?php if($santri['jenis_kelamin'] == 'laki-laki') echo 'checked' ?>>
+  <label for="laki">Laki - laki</label><br>
+  <input type="radio" id="perempuan" name="jenis_kelamin" value="perempuan" <?php if($santri['jenis_kelamin'] == 'perempuan') echo 'checked' ?>>
+  <label for="perempuan">Perempuan</label><br><br>
+  
+  <label for="phone">No handphone orang tua:</label><br>
+  <input type="text" id="phone" name="hp_orangtua" value="<?= $santri['hp_orangtua']?>"><br><br>
+
+  <label for="kelas">Kelas</label><br>
+  <select name="kelas">
+    <option value="wustho-1" <?php if($santri['kelas'] == 'wustho-1') echo 'selected' ?>>Wustho 1</option>
+    <option value="wustho-2" <?php if($santri['kelas'] == 'wustho-2') echo 'selected' ?>>Wustho 2</option>
+    <option value="wustho-3" <?php if($santri['kelas'] == 'wustho-3') echo 'selected' ?>>Wustho 3</option>
+  </select><br><br>
+  <label for="alamat">Alamat:</label><br>
+  <textarea id="alamat" name="alamat" rows="4"><?= $santri['alamat']?></textarea><br><br>
+
+  <input type="submit" name="edit" value="edit"><br><br>
+</form><br><br>
+
+<a href="index.php">Kembali</a>
+
+</body>
+</html>
